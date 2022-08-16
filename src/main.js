537ea89a86122824f9667b542505434992722a51
woGadi <myhiawo@163.com>
2022-08-16 17:26:21 +0800

    初步梳理项目结构，并创建Home组件

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 导入 vant 组件
+import '@/plugins/vant.js'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App)
 }).$mount('#app')
---
+++ src/plugins/vant.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+// 导入 vant 样式表
+import 'vant/lib/index.less'
+// 按需导入 vant 组件
+import { Button } from 'vant'
+
+Vue.use(Button)
